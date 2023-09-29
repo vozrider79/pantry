7976e914b9a90a5cee5aa9edcba86bff69426b52
Jacob Heider <jacob@tea.xyz>
2023-09-28 22:29:28 -0400

    wip

--- projects/gnu.org/glibc/test.c
+++ projects/gnu.org/glibc/test.c
@@ -1,30 +1,8 @@
 #define _GNU_SOURCE
-#include <assert.h>
 #include <gnu/libc-version.h>
-#include <stdatomic.h>
 #include <stdio.h>
-#include <threads.h>
-
-atomic_int acnt;
-int cnt;
-
-int f(void* thr_data) {
-    for(int n = 0; n < 1000; ++n) {
-        ++cnt;
-        ++acnt;
-    }
-    return 0;
-}
 
 int main(int argc, char **argv) {
     /* Basic library version check. */
     printf("gnu_get_libc_version() = %s\n", gnu_get_libc_version());
-
-    thrd_t thr[10];
-    for(int n = 0; n < 10; ++n)
-        thrd_create(&thr[n], f, NULL);
-    for(int n = 0; n < 10; ++n)
-        thrd_join(thr[n], NULL);
-    printf("The atomic counter is %u\n", acnt);
-    printf("The non-atomic counter is %u\n", cnt);
 }
